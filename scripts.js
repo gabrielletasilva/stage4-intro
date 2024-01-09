fbfb663efcac2b474434ea9de65cf13642c25496
Gabrielle <82898190+gabrielletasilva@users.noreply.github.com>
2024-01-09 18:02:48 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Manipulando Strings e Números
+
+// Transformar String em número e número em string
+
+let string = "123"
+console.log(Number(string))
+
+let number = 321
+console.log(String(number))
+
+
+
